99987e1408a99ed91dcef454845510bf65eb7f7a
Arshad Hannan <owlfly360@gmail.com>
2026-04-17 02:12:52 +0530

    navigation completed

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,24 +15,23 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
- const HomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
- @override
-  State<HomePage> createState() => _HomePageState();
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
-  double horizontalPadding = 50.0;
-  double horizontalMargin = 20.0;
-
-  late double position = 40.0;
+  double horizontalPadding = 40.0;
+  double horizontalMargin = 0.0;
+  int noOfIcons = 3;
 
   List<String> icons = [
     'assets/home.png',
@@ -41,104 +39,205 @@ class _HomePageState extends State<HomePage>{
     'assets/info.png'
   ];
 
-  @override
-  Widget build(BuildContext context){
+  late double position;
+  int selected = 0;
 
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 375));
+
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    animation = Tween(
+        begin: getEndPosition(
+            0, horizontalPadding, horizontalMargin, noOfIcons),
+        end: getEndPosition(
+            0, horizontalPadding, horizontalMargin, noOfIcons))
+        .animate(controller);
+    position =
+        getEndPosition(0, horizontalPadding, horizontalMargin, noOfIcons);
+    super.didChangeDependencies();
+  }
+
+  double getEndPosition(int index, double horizontalPadding,
+      double horizontalMargin, int noOfIcons) {
+    double totalMargin = 2 * horizontalMargin;
+    double totalPadding = 2 * horizontalPadding;
+    double valueToOmit = totalMargin + totalPadding;
+
+    return (((MediaQuery.of(context).size.width - valueToOmit) / noOfIcons) *
+        index +
+        horizontalPadding) +
+        (((MediaQuery.of(context).size.width - valueToOmit) / noOfIcons) / 2) -
+        70;
+  }
+
+  void animateDrop(int index) {
+    animation = Tween(
+        begin: position,
+        end: getEndPosition(
+            index, horizontalPadding, horizontalMargin, noOfIcons))
+        .animate(
+        CurvedAnimation(parent: controller, curve: Curves.easeOutBack));
+    controller.forward().then((value) {
+      position =
+          getEndPosition(index, horizontalPadding, horizontalMargin, noOfIcons);
+      controller.dispose();
+      controller = AnimationController(
+          vsync: this, duration: const Duration(milliseconds: 575));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-    body: Stack(
-      children:[
-        Positioned(
-          bottom: horizontalMargin,
-          left: horizontalMargin,
-          child: CustomPaint(
-            painter: AppBarPainter(position),
-            size: Size(MediaQuery.of(context).size.width - (2*horizontalMargin), 80.0),
-            child: SizedBox(
-              height: 120.0,
-              width: MediaQuery.of(context).size.width - (2*horizontalMargin),
-              child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-                  child: Row(
-                    children: icons.map<Widget>((icon) {
-                      return AnimatedContainer(
-                        duration: const Duration(milliseconds: 575),
-                        curve: Curves.easeOut,
-                        height: 105.0,
-                        width: (MediaQuery.of(context).size.width -
-                            (2 * horizontalMargin) -
-                            (2 * horizontalPadding)) /
-                            3,
-                        padding: const EdgeInsets.only(top: 17.5, bottom: 22.5),
-                        alignment: Alignment.bottomCenter,
-                        child: SizedBox(
-                          height: 35.0,
-                          width: 35.0,
-                          child: Center(
-                            child: Image.asset(
-                              icon,
-                              width: 30.0,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      );
-                    }).toList(),
-                  ),
-             ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              color: const Color(0xFF101010),
             ),
-          )
-        )
-      ],
-    ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: AnimatedBuilder(
+              animation: controller,
+              builder: (context, child) {
+                return CustomPaint(
+                  painter: AppBarPainter(animation.value),
+                  size: Size(
+                      MediaQuery.of(context).size.width -
+                          (2 * horizontalMargin),
+                      100.0),
+                  child: SizedBox(
+                    height: 120.0,
+                    width: MediaQuery.of(context).size.width -
+                        (2 * horizontalMargin),
+                    child: Padding(
+                      padding:
+                      EdgeInsets.symmetric(horizontal: horizontalPadding),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: icons.map<Widget>((icon) {
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                animateDrop(icons.indexOf(icon));
+                                selected = icons.indexOf(icon);
+                              });
+                            },
+                            child: AnimatedContainer(
+                              duration: const Duration(milliseconds: 375),
+                              curve: Curves.easeOut,
+                              height: 105,
+                              width: (MediaQuery.of(context).size.width -
+                                  (2 * horizontalMargin) -
+                                  (2 * horizontalPadding)) /
+                                  3,
+                              padding: const EdgeInsets.only(
+                                  bottom: 40.0),
+                              alignment: selected == icons.indexOf(icon)
+                                  ? Alignment.topCenter
+                                  : Alignment.bottomCenter,
+                              child: SizedBox(
+                                height: 35.0,
+                                width: 35.0,
+                                child: Center(
+                                  child: AnimatedSwitcher(
+                                    duration: const Duration(milliseconds: 575),
+                                    switchInCurve: Curves.easeOut,
+                                    switchOutCurve: Curves.easeOut,
+                                    child: selected == icons.indexOf(icon)
+                                        ? Image.asset(
+                                      icon,
+                                      key: ValueKey('yellow$icon'),
+                                      width: 30.0,
+                                      color: const Color(0xFF101010),
+                                    )
+                                        : Image.asset(
+                                      icon,
+                                      key: ValueKey('white$icon'),
+                                      width: 30.0,
+                                      color: const Color(0xFF9D9D9D),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-class AppBarPainter extends CustomPainter{
-
+class AppBarPainter extends CustomPainter {
   double x;
+
   AppBarPainter(this.x);
 
   double height = 80.0;
-  double start = 40.0;
-  double end = 120.0;
+  double start = 20.0;
+  double end = 120;
 
   @override
-  void paint(Canvas canvas, Size size){
+  void paint(Canvas canvas, Size size) {
     var paint = Paint()
-        ..color = Colors.black
-        ..style = PaintingStyle.fill;
+      ..color = const Color(0xFF1D1D1D)
+      ..style = PaintingStyle.fill;
 
-    var path = Path();
+    Path path = Path();
     path.moveTo(0.0, start);
 
-    path.lineTo((x < 20.0 ? 20.0 : x), start);
+    /// DROP paths, included X for animation
+    path.lineTo((x) < 20.0 ? 20.0 : x, start);
     path.quadraticBezierTo(20.0 + x, start, 30.0 + x, start + 30.0);
     path.quadraticBezierTo(40.0 + x, start + 55.0, 70.0 + x, start + 55.0);
     path.quadraticBezierTo(100.0 + x, start + 55.0, 110.0 + x, start + 30.0);
-    path.quadraticBezierTo(120.0 + x, start, (140.0 + x) > (size.width - 20.0) ? (size.width - 20.0) : 140.0 + x, start);
-
+    path.quadraticBezierTo(
+        120.0 + x,
+        start,
+        (140.0 + x) > (size.width - 20.0) ? (size.width - 20.0) : 140.0 + x,
+        start);
     path.lineTo(size.width - 20.0, start);
 
+    /// Box with BorderRadius
     path.quadraticBezierTo(size.width, start, size.width, start + 25.0);
     path.lineTo(size.width, end - 25.0);
-    path.quadraticBezierTo(size.width, end, size.width -25.0 , end);
+    path.quadraticBezierTo(size.width, end, size.width - 25.0, end);
     path.lineTo(25.0, end);
     path.quadraticBezierTo(0.0, end, 0.0, end - 25.0);
     path.lineTo(0.0, start + 25.0);
     path.quadraticBezierTo(0.0, start, 20.0, start);
     path.close();
 
-
     canvas.drawPath(path, paint);
 
-    canvas.drawCircle(Offset(70.0+ x, 50.0), 35.0, paint);
-
+    /// Circle to show at the top of the drop
+    var circlePaint = Paint()
+      ..color = const Color(0xFF59B655)
+      ..style = PaintingStyle.fill;
+    canvas.drawCircle(Offset(x + 70.0, 30.0), 35.0, circlePaint);
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate){
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
   }
-
 }
